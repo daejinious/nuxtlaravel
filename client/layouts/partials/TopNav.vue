bd4d0e4572a41a0ad6970179b4e5ce0823f8f056
cl-djkim <djkim@commercelab.co.kr>
2020-05-20 14:48:32 +0900

    Apply .gitignore

---
+++ client/layouts/partials/TopNav.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <div class="container">
+        <nuxt-link class="navbar-brand" to="/">Jwt-Auth</nuxt-link>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/dashboard">Dashboard</nuxt-link>
+            </li>
+          </b-navbar-nav>
+
+          <b-navbar-nav class="ml-auto">
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/login">Login</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/register">Register</nuxt-link>
+            </li>
+          </b-navbar-nav>
+        </b-collapse>
+      </div>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TopNav"
+  }
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Home!!!</h1>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
